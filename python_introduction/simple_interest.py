367576a6dfa7c0c48f89715b362fc096b168cfb8
pluto476526 <ngugikibuka99@gmail.com>
2024-09-11 00:29:11 +0300

    Simple interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Variables to calculate simple interest
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate interest
+interest = principal * rate * time
+
+print('The simple interest is :', interest)
