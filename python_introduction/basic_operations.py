2dcf656de83ba28a636dec4954a8e65109dde2b7
pluto476526 <ngugikibuka99@gmail.com>
2024-09-11 00:13:37 +0300

    Basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define variables
+number1 = 10
+number2 = 5
+
+# Perform calculations
+addition = number1 + number2
+subtraction = number1 - number2
+product = number1 * number2
+
+# Show results
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", product)
