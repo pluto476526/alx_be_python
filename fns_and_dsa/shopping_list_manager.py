19919f4cd3e720a4292c81c861b9c467962d6cb4
pluto476526 <ngugikibuka99@gmail.com>
2024-09-24 14:12:54 +0300

    Modified prompts

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,11 +14,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} added to list.")
         elif choice == '2':
-            item = input("Enter item: ")
+            item = input("Enter the item to remove: ")
             if item not in shopping_list:
                 print(f"{item} not found.")
             else:
