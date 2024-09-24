d7aa489ce937ef3dcee5ebeb7471df3148431b2b
pluto476526 <ngugikibuka99@gmail.com>
2024-09-24 13:48:23 +0300

    Modified func

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
     if operation == 'add':
-        result = num1 + num2
+        return num1 + num2
     elif operation == 'subtract':
-        result = num1 - num2
+        return num1 - num2
     elif operation == 'multiply':
-        result = num1 * num2
+        return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
             print('Cannot divide by 0.')
         else:
-            result = num1 / num2
+            return num1 / num2
     else:
         print('Invalid operation.')
