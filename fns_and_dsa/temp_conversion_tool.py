89212c1b878fa98d00e6d08def3e4a3ec4ece30d
pluto476526 <ngugikibuka99@gmail.com>
2024-09-24 15:07:14 +0300

    Fixed spelling

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,19 +1,19 @@
-FAHRENHEIT_TO_CELCIUS_FACTOR = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELCIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celcius(fahrenheit):
-    return fahrenheit * FAHRENHEIT_TO_CELCIUS_FACTOR
+def convert_to_celsius(fahrenheit):
+    return fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-def convert_to_fahrenheit(celcius):
-    return celcius * CELCIUS_TO_FAHRENHEIT_FACTOR
+def convert_to_fahrenheit(celsius):
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
 
 if __name__ == "__main__":
     temp = int(input("Enter the temperature to convert: "))
-    conv = input("Is this temperature in Celcius or Fahrenheit? (C/F): ")
+    conv = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower
 
     if conv == 'c':
         print(temp, "C is", convert_to_fahrenheit(temp), "F")
     elif conv == 'f':
-        print(temp, "F is", convert_to_celcius(temp), "C")
+        print(temp, "F is", convert_to_celsius(temp), "C")
     else:
         print("Invalid operation.")
