a01f350cd8ee294129803cabf5f063b70d1780bf
pluto476526 <ngugikibuka99@gmail.com>
2024-09-24 15:26:22 +0300

    Added error message

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -7,13 +7,23 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
+def is_int(val):
+    try:
+        int(val)
+        return True
+    except valErr:
+        return False
+
 if __name__ == "__main__":
     temp = int(input("Enter the temperature to convert: "))
     conv = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
 
-    if conv == 'c':
-        print(temp, "C is", convert_to_fahrenheit(temp), "F")
-    elif conv == 'f':
-        print(temp, "F is", convert_to_celsius(temp), "C")
+    if is_int(temp):
+        if conv == 'c':
+            print(temp, "C is", convert_to_fahrenheit(temp), "F")
+        elif conv == 'f':
+            print(temp, "F is", convert_to_celsius(temp), "C")
+        else:
+            print("Invalid operation.")
     else:
-        print("Invalid operation.")
+        print("Invalid temperature. Please enter a numeric value.")
