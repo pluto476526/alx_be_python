1e4469e6db5d770c76164d167479a210dc7d9f94
pluto476526 <ngugikibuka99@gmail.com>
2024-10-13 14:11:28 +0300

    Auto commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,13 +3,16 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title}, by {self.author}"
+
 class EBook(Book):
     def __init__(self, title:str, author:str, file_size:int):
         super().__init__(title, author)
         self.file_size = file_size
     
     def __str__(self):
-        return f"{self.title} by {self.author}, File Size: {self.file_size}"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title:str, author:str, page_count:int):
@@ -17,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"Book: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
