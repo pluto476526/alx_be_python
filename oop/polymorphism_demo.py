052b9a339da7fb3be5aece73434c1bd1fa2f2f4a
pluto476526 <ngugikibuka99@gmail.com>
2024-10-13 17:55:38 +0300

    Auto commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Derived classes oeride this method")
+
+class Rectangle(Shape):
+    def __init__(self, length:int, width:int):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(shape):
+    def __init__(self, radius:int):
+        self.radius = radius
+
+    def area(self):
+        return self.radius ** 2 * math.pi
